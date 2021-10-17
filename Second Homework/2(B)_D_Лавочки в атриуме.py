4ac74dc89a45b13966666789be4c7c75948fdf4d
7Dany6 <dany.sulimov@yandex.ru>
2021-10-17 18:43:49 +0300

    Added solution for fifth problem

--- Second Homework/2(B)_D_Лавочки в атриуме.py
+++ Second Homework/2(B)_D_Лавочки в атриуме.py
@@ -1,18 +1,16 @@
-L, K = map(int, input().split())
-middle = len(L) / 2
-to_delete = 0
-for _ in range(K):
-    arguments = list(map(int, input().split()))
-if K == 1:
-    print(arguments[0])
-else:
-    for i in range(K):
-        if i < middle:
-            to_delete = i
-        elif i == middle:
-            if len % 2 != 0:
-                print(arguments[to_delete])
-            else:
-                print(f'{arguments[to_delete]} {arguments[i]}')
+def count(length, number_of_legs, array_legs):
+    to_save = 0
+    middle = length // 2
+    if len(array_legs) == 1:
+        return array_legs[0]
+    for leg in range(number_of_legs):
+        if array_legs[leg] < middle:
+            to_save = leg
+        elif array_legs[leg] == middle and length % 2 != 0:
+                return array_legs[leg]
         else:
-            print(f'{arguments[to_delete]} {arguments[i]}')
+            return '{} {}'.format(array_legs[to_save], array_legs[leg])
+
+L, K = map(int, input().split())
+legs = list(map(int, input().split()))
+print(count(L, K, legs))
